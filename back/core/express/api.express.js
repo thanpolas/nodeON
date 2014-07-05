7759a0705754a6baf521afa7ddfc880a731311e8
Thanasis Polychronakis <thanpolas@gmail.com>
2014-07-05 18:11:06 +0300

    work on refactoring express boot sequence

--- back/core/express/api.express.js
+++ back/core/express/api.express.js
@@ -1,3 +1,82 @@
 /**
  * @fileOverview API express instance and configuration.
  */
+var path = require('path');
+
+var express = require('express');
+var flash = require('connect-flash');
+var errorhandler = require('errorhandler');
+var Promise = require('bluebird');
+
+var log = require('logg').getLogger('app.core.express.api');
+
+var webserver = require('./webserver.core');
+var socketServer = require('./websocketServer.core').getInstance();
+var session = require('./session-store.core').getInstance();
+var authMidd = require('../middleware/auth.midd').getInstance();
+var corsMidd = require('../middleware/cors.midd').getInstance();
+var webRouter = require('../routes/web.router');
+var globals = require('./globals');
+var ExpressApp = require('./base.express.js');
+
+var ApiExpress = module.exports = ExpressApp.extendSingleton(function () {
+
+});
+
+/**
+ * Kick off the webserver...
+ *
+ * @param {Object} opts Options as defined in app.init().
+ * @return {Promise(express)} a promise with the express instance.
+ */
+ApiExpress.prototype.init = Promise.method(function(opts) {
+  log.info('init() :: Initializing webserver...');
+
+  if (this.app !== null) {
+    return this.app;
+  }
+
+  this.app = express();
+
+  // Setup express
+  this.baseSetup();
+
+  this.app.set('views', __dirname + '/../../front/templates');
+  this.app.set('view engine', 'jade');
+
+  // enable CORS for current development flow.
+  this.app.use(corsMidd.allowCrossDomain.bind(corsMidd));
+
+  // Session store
+  var sessConnectPromise = session.connect();
+  this.app.use(session.use());
+
+  // use flashing for passing messages to next page view
+  this.app.use(flash());
+  this.app.use(express.static(path.join(__dirname, '/../../front/static')));
+
+  // initialize authentication
+  authMidd.init(this.app);
+
+  // initialize webserver
+  webserver.init(this.app);
+
+  // add the routes
+  webRouter.init(this.app, opts);
+
+  // Init websockets
+  socketServer.init(webserver.http);
+
+  // development only
+  if (globals.isDev) {
+    this.app.use(errorhandler());
+  }
+
+  // setup view globals
+  this.app.locals.glob = globals.viewGlobals;
+
+  return Promise.all([
+    webserver.start(this.app),
+    sessConnectPromise,
+  ]);
+});
--- back/core/express/core.express.js
+++ back/core/express/core.express.js
@@ -1,100 +1,51 @@
 /**
- * @fileOverview Initialize webserver services, express, routes, etc
+ * @fileOverview Base Ctor for express instances, provides helpers, wrappers.
  */
 var path = require('path');
 
+var cip = require('cip');
 var config = require('config');
 var Promise = require('bluebird');
-var express = require('express');
 var cookieParser = require('cookie-parser');
-var flash = require('connect-flash');
-var log = require('logg').getLogger('app.core.express');
-var errorhandler = require('errorhandler');
 var bodyParser = require('body-parser');
 
-var webserver = require('./webserver.core');
-var socketServer = require('./websocketServer.core').getInstance();
-var session = require('./session-store.core').getInstance();
-var authMidd = require('../middleware/auth.midd').getInstance();
-var corsMidd = require('../middleware/cors.midd').getInstance();
-var webRouter = require('../routes/web.router');
+// var log = require('logg').getLogger('app.core.express');
+
 var globals = require('./globals');
 
-var expressApp = module.exports = {};
+/**
+ * Base Ctor for express instances, provides helpers, wrappers.
+ *
+ * @constructor
+ */
+var ExpressApp = module.exports = cip.extend(function() {
+  /** @type {?express} The express instance */
+  this.app = null;
 
-/** @type {?express} The express instance */
-expressApp.app = null;
+  /** @type {app.core.SessionStore} Instance of Session Store */
+  this.sessionStore = null;
+});
 
-/** @type {app.core.SessionStore} Instance of Session Store */
-expressApp.sessionStore = null;
 
 /**
- * Kick off the webserver...
+ * Basic setup of express instance.
  *
- *
- * @param {Object} opts Options as defined in app.init().
  * @return {Promise} a promise.
  */
-expressApp.init = Promise.method(function(opts) {
-  log.info('init() :: Initializing webserver...');
-
-  if (expressApp.app !== null) {
-    return;
-  }
-
-  var app = expressApp.app = express();
-
+ExpressApp.prototype.baseSetup = Promise.method(function() {
   // Discover proper port, Heroku exports it in an env
   var port;
   if (globals.isHeroku) {
     port = process.env.PORT;
   } else {
-    port = globals.isApi ? config.webserver.apiPort : config.webserver.port;
+    port = config.webserver.port;
   }
 
   // Setup express
-  app.set('port', port);
-  app.set('views', __dirname + '/../../front/templates');
-  app.set('view engine', 'jade');
+  this.app.set('port', port);
   // remove x-powered-by header
-  app.set('x-powered-by', false);
+  this.app.set('x-powered-by', false);
 
-  // enable CORS for current development flow.
-  app.use(corsMidd.allowCrossDomain.bind(corsMidd));
-
-  app.use(cookieParser());
-  app.use(bodyParser.json());
-
-  // Session store
-  var sessConnectPromise = session.connect();
-  app.use(session.use());
-
-  // use flashing for passing messages to next page view
-  app.use(flash());
-  app.use(express.static(path.join(__dirname, '/../../front/static')));
-
-  // initialize authentication
-  authMidd.init(app);
-
-  // initialize webserver
-  webserver.init(app);
-
-  // add the routes
-  webRouter.init(app, opts);
-
-  // Init websockets
-  socketServer.init(webserver.http);
-
-  // development only
-  if (globals.isDev) {
-    app.use(errorhandler());
-  }
-
-  // setup view globals
-  app.locals.glob = globals.viewGlobals;
-
-  return Promise.all([
-    webserver.start(app),
-    sessConnectPromise,
-  ]);
+  this.app.use(cookieParser());
+  this.app.use(bodyParser.json());
 });
