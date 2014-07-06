a3d804ffd8bf0ad9a849503c17c47a032ab08d84
Thanasis Polychronakis <thanpolas@gmail.com>
2014-07-06 12:50:42 +0300

    include API vhost in website tests

--- test/website/frontpage.test.js
+++ test/website/frontpage.test.js
@@ -17,12 +17,24 @@ describe('Frontpage', function() {
 
   Web.setup();
 
-  it('Will get a 200 on the frontpage', function(done) {
+  it('Will get a 200 on the website frontpage', function(done) {
     this.req.get('/')
       .expect(200, done);
   });
-  it('Will not contain the "x-powered-by" header"', function(done) {
+  it('Will get a 200 on the api frontpage', function(done) {
+    this.reqApi.get('/')
+      .expect(200, done);
+  });
+
+  it('Will not contain the "x-powered-by" header on website"', function(done) {
     this.req.get('/')
+      .end(function(err, req) {
+        expect(req.header).to.not.have.property('x-powered-by');
+        done();
+      });
+  });
+  it('Will not contain the "x-powered-by" header on api"', function(done) {
+    this.reqApi.get('/')
       .end(function(err, req) {
         expect(req.header).to.not.have.property('x-powered-by');
         done();
