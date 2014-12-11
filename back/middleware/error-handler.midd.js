10a967d77c3d1bf5d169802a107165492e553a5e
Thanasis Polychronakis <thanpolas@gmail.com>
2014-12-11 12:18:43 +0200

    Scaffold the Error handler

---
+++ back/middleware/error-handler.midd.js
@@ -0,0 +1,47 @@
+/**
+ * @fileOverview Master Express Error Handler.
+ */
+var appError = require('nodeon-error');
+
+var log = require('logg').getLogger('app.midd.errorHandler');
+
+var err = module.exports = {};
+
+/**
+ * The master Error Handler.
+ *
+ * @param {Error} err The error thrown.
+ * @param {Object} req The request Object.
+ * @param {Object} res The response Object.
+ * @param {Function(Error=)} next passing control to the next middleware.
+ */
+/*jshint unused:false */
+err.handle = function (err, req, res, next) {
+  var status = 500;
+  var error;
+
+  if (typeof err === 'string') {
+    var tmpErr = new appError(err);
+    error = tmpErr.toApi();
+  } else {
+    status = err.status || status;
+    if (typeof err.toApi === 'function') {
+      error = err.toApi();
+    } else {
+      error = err;
+    }
+  }
+
+  res.status(status);
+  log.warn('handle() :: Handling error, status:', status, 'Message:',
+    error.message);
+  log.finest('handle() :: Raw error object:', err);
+
+  // figure out what the client accepts
+  if (req.headers['accept'] && req.headers['accept'].match(/json/)) {
+    res.json(error);
+  } else {
+    res.render('error/500', {error: error});
+  }
+
+};
